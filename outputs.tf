cad54b503d39eece7a7cbe26e4f7ba4ebf5ef623
Paul <paul@noxian.net>
2023-06-24 12:09:59 -0400

    removed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,1 @@
-output "mx_domain" {
-  description = "The domain of the mx records."
-  value = cloudflare_record.mx.domain
-}
-
-output "creation_date" {
-  description = "Creation date of the record"
-  value = cloudflare_record.mx.created_on
-}
 
